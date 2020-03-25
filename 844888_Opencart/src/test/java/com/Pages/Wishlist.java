84e932c99b86d86d91db93f8e23906e337134285
maneesh <maneesh@LAPTOP-CS1U9J40>
2020-03-25 13:12:29 +0530

    Updated

--- 844888_Opencart/src/test/java/com/Pages/Wishlist.java
+++ 844888_Opencart/src/test/java/com/Pages/Wishlist.java
@@ -2,15 +2,10 @@ package com.Pages;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 import baseClass.Wrapper;
 
 public class Wishlist extends Wrapper {
