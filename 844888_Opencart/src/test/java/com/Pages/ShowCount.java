febe3d0da304758b5d36febe707b6e39b3ba7bb0
maneesh <maneesh@LAPTOP-CS1U9J40>
2020-03-25 12:58:16 +0530

    Updated

--- 844888_Opencart/src/test/java/com/Pages/ShowCount.java
+++ 844888_Opencart/src/test/java/com/Pages/ShowCount.java
@@ -2,16 +2,12 @@ package com.Pages;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 import baseClass.Wrapper;
