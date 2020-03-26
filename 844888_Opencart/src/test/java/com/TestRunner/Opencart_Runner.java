0f6bcc20e0e898afaaba63a850e92e5a913a4e32
maneesh <maneesh@LAPTOP-CS1U9J40>
2020-03-26 14:22:16 +0530

    Updated

--- 844888_Opencart/src/test/java/com/TestRunner/Opencart_Runner.java
+++ 844888_Opencart/src/test/java/com/TestRunner/Opencart_Runner.java
@@ -1,6 +1,9 @@
 package com.TestRunner;
 
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+
+import com.cucumber.listener.Reporter;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
@@ -8,11 +11,19 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "src\\main\\resources\\Featurefolder\\project.feature",
-		plugin = {"pretty", "html:reports/cucumber-html-reports"},
+		plugin = {"pretty", "html:reports/cucumber-html-reports","json:reports/cucumber-html-report/jsonreport","com.cucumber.listener.ExtentCucumberFormatter:reports/Extentreports/Extentreport.html"},
 		tags = {"@TC01_Selectingitem,@TC02_Shownumber,@TC03_Sorting,@TC04_Storetoexcelsheet,@TC05_Storetowishlist"},
 		glue = {"com.StepDefinition"},
 		monochrome = true
 		)
 public class Opencart_Runner {  
 
+	@AfterClass
+	  public static void extendReport() 
+	{
+	      Reporter.loadXMLConfig("src/test/resources/extent-config.xml");
+	      Reporter.setSystemInfo("user", System.getProperty("user.name"));
+	      Reporter.setSystemInfo("os", "Windows");
+	      Reporter.setTestRunnerOutput("Sample test runner output message");
+	}
 }
